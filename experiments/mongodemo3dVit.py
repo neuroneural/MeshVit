4e6cfeb90787e20d43db5e27adc2985330bcabc1
William Ashbee <wsashbee@gmail.com>
2023-09-06 19:17:58 +0000

    seems to be working

--- experiments/mongodemo3dVit.py
+++ experiments/mongodemo3dVit.py
@@ -31,6 +31,44 @@ from segmenter.segm.model.vit3d import VisionTransformer3d
 from subvolume.utils import extract_subvolumes
 import torch.nn.functional as F
 
+import argparse
+import os
+
+filename = '/data/users2/washbee/MeshVit/experiments/hyperparam.log'
+
+def log_hyperparams(subvolume_size, patch_size, n_layers, d_model, d_ff, n_heads, d_encoder, completed, msg,loss):
+    # Check if the file exists
+    if not os.path.exists(filename):
+        with open(filename, 'w') as file:
+            # Write the header
+            file.write("subvolume_size,patch_size,n_layers,d_model,d_ff,n_heads,d_encoder,completed,msg,loss\n")
+    
+    # Append the data
+    with open(filename, 'a') as file:
+        file.write(f"{subvolume_size},{patch_size},{n_layers},{d_model},{d_ff},{n_heads},{d_encoder},{completed},{msg},{loss}\n")
+
+parser = argparse.ArgumentParser(description='Hyperparameters for 3D Vision Transformer')
+parser.add_argument('--subvolume_size', type=int, required=True)
+parser.add_argument('--patch_size', type=int, required=True)
+parser.add_argument('--n_layers', type=int, required=True)
+parser.add_argument('--d_model', type=int, required=True)
+parser.add_argument('--d_ff', type=int, required=True)
+parser.add_argument('--n_heads', type=int, required=True)
+parser.add_argument('--d_encoder', type=int, required=True)
+
+args = parser.parse_args()
+
+# Replace original hyperparameter declarations with the following
+subvolume_size = args.subvolume_size
+patch_size = args.patch_size
+n_layers = args.n_layers
+d_model = args.d_model
+d_ff = args.d_ff
+n_heads = args.n_heads
+d_encoder = args.d_encoder
+
+
+
 volume_shape = [256]*3
 subvolume_shape = [32]*3 # if you are sampling subcubes within the
                          # volume. Since you will have to generate patches for
@@ -109,13 +147,14 @@ tdataloader = BatchPrefetchLoaderWrapper(
 config_file = 'modelAE.json'
 
 device = "cuda"
-subvolume_size = 64
+#now as args above:
+#subvolume_size = 64
 image_size = (subvolume_size,subvolume_size,subvolume_size)
-patch_size = 16 
-n_layers = 12
-d_model = 128
-d_ff = 128
-n_heads = 8
+#patch_size = 16 
+#n_layers = 12
+#d_model = 128
+#d_ff = 128
+#n_heads = 8
 n_cls = 104 #per voxel number of classes
 vit = VisionTransformer3d(image_size, patch_size, n_layers, d_model, d_ff, n_heads, n_cls, 
         dropout=0.1,
@@ -123,59 +162,75 @@ vit = VisionTransformer3d(image_size, patch_size, n_layers, d_model, d_ff, n_hea
         distilled=False,
         channels=1)
 
-d_encoder = 128
+#d_encoder = 128
 #...
 drop_path_rate=0.0
 dropout=0.1
-decoder = MaskTransformer3d(n_cls, patch_size,d_encoder, n_layers, n_heads, d_model, d_ff, drop_path_rate, dropout)
-model = Segmenter3d(vit, decoder, n_cls=n_cls).to(device)
+loss = None
+completed = False
+try:
+    decoder = MaskTransformer3d(n_cls, patch_size,d_encoder, n_layers, n_heads, d_model, d_ff, drop_path_rate, dropout)
+    model = Segmenter3d(vit, decoder, n_cls=n_cls).to(device)
 
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-model = model.to(device)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model = model.to(device)
 
-criterion = nn.CrossEntropyLoss()  # for segmentation tasks
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    criterion = nn.CrossEntropyLoss()  # for segmentation tasks
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-num_epochs = 1  # or however many you want
+    num_epochs = 1  # or however many you want
 
-model.train()  # set the model to training mode
+    model.train()  # set the model to training mode
 
-coordinates = coord_generator.get_coordinates(mode="train")
+    coordinates = coord_generator.get_coordinates(mode="train")
 
-for epoch in range(num_epochs):
-    print(f"Epoch {epoch + 1}/{num_epochs}")
-    
-    for i, (x, y) in enumerate(tdataloader):
-        # Move data to GPU if available
-        x, y = x.cuda(), y.cuda()
+    for epoch in range(num_epochs):
+        print(f"Epoch {epoch + 1}/{num_epochs}")
         
-        
-
-        subvolumesx = extract_subvolumes(x, subvolume_size)
-        subvolumesy = extract_subvolumes(y, subvolume_size)
-        # Forward pass
-        for subvolx,subvoly in zip(subvolumesx,subvolumesy):
+        for i, (x, y) in enumerate(tdataloader):
+            # Move data to GPU if available
+            x, y = x.cuda(), y.cuda()
             
-            outputs = model(subvolx)
+            
 
-            print('compare',outputs.shape,subvoly.long().shape, y.shape)
+            subvolumesx = extract_subvolumes(x, subvolume_size)
+            subvolumesy = extract_subvolumes(y, subvolume_size)
+            # Forward pass
+            for subvolx,subvoly in zip(subvolumesx,subvolumesy):
+                
+                outputs = model(subvolx)
+
+                print('compare',outputs.shape,subvoly.long().shape, y.shape)
+                loss = criterion(outputs, subvoly.long())  # Ensure the target tensor is of type long
+                # Print some statistics
+                #if (i + 1) % 10 == 0:  # print every 10 batches for example
+                print(f"Batch {i+1}, Loss: {loss.item()}")
+                # Backward pass and optimization
+                optimizer.zero_grad()
+                loss.backward()
+                optimizer.step()
 
 
-            loss = criterion(outputs, subvoly.long())  # Ensure the target tensor is of type long
-
+            #exit()
             # Print some statistics
-            #if (i + 1) % 10 == 0:  # print every 10 batches for example
-            print(f"Batch {i+1}, Loss: {loss.item()}")
-            # Backward pass and optimization
-            optimizer.zero_grad()
-            loss.backward()
-            optimizer.step()
+            if (i + 1) % 10 == 0:  # print every 10 batches for example
+                print(f"Batch {i+1}, Loss: {loss.item()}")
+                print(x.shape,y.shape)
+                
+            easybar.print_progress(i, len(tdataloader))
+    
+    
+    log_hyperparams(subvolume_size, patch_size, n_layers, d_model, d_ff, n_heads, d_encoder, True, "Completed", loss.item())
+    completed = True
 
-
-        #exit()
-        # Print some statistics
-        if (i + 1) % 10 == 0:  # print every 10 batches for example
-            print(f"Batch {i+1}, Loss: {loss.item()}")
-            print(x.shape,y.shape)
-            
-        easybar.print_progress(i, len(tdataloader))
+except torch.cuda.OutOfMemoryError as e:
+    print("torch.cuda.OutOfMemoryError caught")
+except Exception as e:
+    print('exception caught')
+except BaseException as e:
+    print('base exception caught')
+finally:
+    print("finally")
+    if not completed:
+        log_hyperparams(subvolume_size, patch_size, n_layers, d_model, d_ff, n_heads, d_encoder, False, "Not Completed", "NA")
+    exit()
