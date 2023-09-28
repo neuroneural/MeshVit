312983f68ead734b0a09adf22209c6299d1a3e52
William Ashbee <wsashbee@gmail.com>
2023-09-28 20:35:01 +0000

    bad 3dvit

--- experiments/figures/plotSegmentedSubvolumes.py
+++ experiments/figures/plotSegmentedSubvolumes.py
@@ -7,14 +7,21 @@ import matplotlib.pyplot as plt
 from mongoutil import *
 from fixed_coords_generator import FixedCoordGenerator
 # 1. Get 5 MRI volumes
-batch_size = 1 
+batch_size = 1
 mongo_loader = MongoDataLoader(batch_size=batch_size)
 _, _, test_loader = mongo_loader.get_mongo_loaders()
 
 
 data_iter = iter(test_loader)
 mri_tensors, label_tensors = next(data_iter)
-
+print('Database')
+print("mri_tensors.shape","label_tensors.shape",mri_tensors.shape,label_tensors.shape)
+#print('Fake')
+#mri_tensors = torch.zeros((1,1,256,256,256),dtype=torch.float32)
+#label_tensors = torch.zeros((1,256,256,256),dtype=torch.int64)
+#print("mri_tensors.shape","label_tensors.shape",mri_tensors.shape,label_tensors.shape)
+assert mri_tensors.shape == (1,1,256,256,256) 
+assert label_tensors.shape == (1,256,256,256) 
 print("MRI Tensor dtype:", mri_tensors.dtype)
 print("Label Tensor dtype:", label_tensors.dtype)
  
@@ -22,15 +29,27 @@ print("Label Tensor dtype:", label_tensors.dtype)
 
 coord_gen = FixedCoordGenerator(256, 128)
 label_subvolumes_list = []
-
+mri_subvolumes_list = []
 print("shapes",mri_tensors.shape,label_tensors.shape)
 for i in range(batch_size):
-    _, label_subvolumes = mongo_loader.extract_all_subvolumes(mri_tensors[i].unsqueeze(0), label_tensors[i].unsqueeze(0), coord_gen)
-    print("MRI subvolumes dtype:", _[0][0].dtype)
+    mri_subvolumes, label_subvolumes = mongo_loader.extract_all_subvolumes(mri_tensors[i].unsqueeze(0), label_tensors[i].unsqueeze(0), coord_gen)
+    print("MRI subvolumes dtype:", mri_subvolumes[0][0].dtype)
     print("Label subvolumes dtype:",  label_subvolumes[0][0].dtype)
 
     label_subvolumes_list.append(label_subvolumes)
-
+    mri_subvolumes_list.append(mri_subvolumes)
+    print(type(mri_subvolumes))
+    print(len(mri_subvolumes))
+    print(type(label_subvolumes))
+    print(len(label_subvolumes))
+    print(type(mri_subvolumes[0]))
+    print(len(mri_subvolumes[0]))
+    print(type(label_subvolumes[0]))
+    print(len(label_subvolumes[0]))
+    print(type(mri_subvolumes[0][0]))
+    print(mri_subvolumes[0][0].shape)
+    print(type(label_subvolumes[0][0]))
+    print(label_subvolumes[0][0].shape)
 print('label_subvolumes_list', len(label_subvolumes_list))
 
 # 3. Reassemble label subvolumes into volume
@@ -56,7 +75,6 @@ print('checkpoint keys')
 #for key in checkpoint:
 #    print(key)
 
-#exit()
 model = nets.UNet(
     dimensions=3,
     in_channels=1,
@@ -76,9 +94,11 @@ model.to(device)
 with torch.no_grad():
     mri_tensors = mri_tensors.to(device)
     predictions = model(mri_tensors)
+    
     predictions = predictions.cpu()  # Move predictions to CPU for further processing
 
 print("predictions.shape", predictions.shape)
+assert predictions.shape == (1,3,256,256,256) #trained on subvolume, but produces full volume. 
 # Apply softmax to the predictions along the channel dimension
 softmax_predictions = torch.nn.functional.softmax(predictions, dim=1)
 
@@ -86,7 +106,7 @@ softmax_predictions = torch.nn.functional.softmax(predictions, dim=1)
 class_predictions = torch.argmax(softmax_predictions, dim=1)
 
 # 4. Display MRI, Original Label, and Prediction
-def display_slices(mri, original_label, predicted_label, slice_num, save_dir='./saved_figs'):
+def display_slices(mri, original_label, predicted_label, slice_num, save_dir='./saved_figs',name = ""):
     if not os.path.exists(save_dir):
         os.makedirs(save_dir)
 
@@ -101,8 +121,10 @@ def display_slices(mri, original_label, predicted_label, slice_num, save_dir='./
         ax.axis('off')
     plt.tight_layout()
 
-    save_path = os.path.join(save_dir, f'slice_{slice_num}.svg')
+    save_path = os.path.join(save_dir, f'{name}_slice_{slice_num}.svg')
     fig.savefig(save_path, format='svg')
+    save_path = os.path.join(save_dir, f'{name}_slice_{slice_num}.png')
+    fig.savefig(save_path, format='png')
     plt.close(fig)
 
 # Displaying the first MRI volume's slice 128
@@ -110,4 +132,68 @@ display_slices(mri_tensors[0][0].cpu().numpy(),
                label_tensors[0].cpu().numpy(), 
                class_predictions[0].cpu().numpy(), 
                128, 
-               save_dir='./unet_figs')
+               save_dir='./unet_figs',name='unet_')
+
+import sys
+sys.path.append('/data/users2/washbee/MeshVit') #change this path
+
+from segmenter.segm.model.decoder3d import MaskTransformer3d  #check sys.path.append
+from segmenter.segm.model.segmenter3d import Segmenter3d #check sys.path.append
+from segmenter.segm.model.vit3d import VisionTransformer3d #check sys.path.append
+
+
+subvolume_shape = [128,128,128]
+patch_size = 16
+n_layers = 8
+d_model = 128
+d_ff = 64
+n_heads = 64
+n_classes = 3
+vit = VisionTransformer3d(subvolume_shape, patch_size, n_layers, d_model, d_ff, n_heads, n_classes, 
+            dropout=0.1,
+            drop_path_rate=0.0,
+            distilled=False,
+            channels=1)
+drop_path_rate=0.0
+dropout=0.1
+d_encoder = 128
+decoder = MaskTransformer3d(n_classes, patch_size, d_encoder, n_layers, n_heads, d_model, d_ff, drop_path_rate, dropout)
+model = Segmenter3d(vit, decoder, n_cls=n_classes)
+        
+model_path = "/data/users2/washbee/MeshVit/experiments/logs/3DVit_e100_gmwm_sv128_8d5db215-95fc-40cd-98aa-824c89a6a03f/best_full.pth"
+checkpoint = torch.load(model_path,map_location=torch.device('cpu'))
+    
+model.load_state_dict(checkpoint['model_state_dict'])
+model.eval()
+
+# If you're using CUDA
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+model.to(device)
+
+# 3.2 Run predictions
+predictions_list = []
+with torch.no_grad():
+    subvolumes = mri_subvolumes_list[0]#.to(device)
+    
+    for subvolume, coord in subvolumes:
+        print("subvolume.shape",subvolume.shape)
+        prediction = model(subvolume)
+        prediction = prediction.cpu()  # Move predictions to CPU for further processing
+        softmax_prediction = torch.nn.functional.softmax(prediction, dim=1)
+        # Get the class prediction by taking the argmax along the channel dimension
+        class_prediction = torch.argmax(softmax_prediction, dim=1)
+        print("class_predictions.shape,coord",class_prediction.shape,coord)
+        predictions_list.append((class_prediction,coord))
+
+
+reconstructed_prediction = mongo_loader.reconstitute_volume(predictions_list, (256, 256, 256))
+
+print("reconstructed_prediction.shape", reconstructed_prediction.shape)
+assert reconstructed_prediction.shape == (256,256,256)
+# Apply softmax to the predictions along the channel dimension
+#exit()
+display_slices(mri_tensors[0][0].cpu().numpy(), 
+               label_tensors[0].cpu().numpy(), 
+               reconstructed_prediction.cpu().numpy(), 
+               128, 
+               save_dir='./3DVit_figs',name='3DVit')
